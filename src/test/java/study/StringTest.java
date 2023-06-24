a56cbb5c8502ada0150bf24a617d30f2a901f529
kmss6905 <kmss69052@naver.com>
2023-06-24 14:49:27 +0900

    #1 - 요구사항 1

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,5 +1,6 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -7,7 +8,16 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class StringTest {
     @Test
     void replace() {
-        String actual = "abc".replace("b", "d");
+        String actual = "abc".replace('b', 'd');
         assertThat(actual).isEqualTo("adc");
+    }
+    @Test
+    void splitTest() {
+        String value1 = "1,2";
+        String value2 = "1";
+        String[] result1 = value1.split(",");
+        String[] result2 = value2.split(",");
+        assertThat(result1).contains("1", "2");
+        assertThat(result2).containsExactly("1");
     }
 }
