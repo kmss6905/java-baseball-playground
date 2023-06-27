12d6d2e1f2329186cc216e868917a6184a725f15
kmss6905 <kmss69052@naver.com>
2023-06-27 18:46:51 +0900

    문자열 계산기 2차 스크립트

--- src/test/java/study/StringCalculatorTest.java
+++ src/test/java/study/StringCalculatorTest.java
@@ -6,10 +6,19 @@ import java.util.Objects;
 
 class StringCalculatorTest {
 
+    /**
+     * 계산식이 포함된 문자열이 들어온다.
+     * 문자열을 공백을 기준으로 나누어 배열에 저장한다.
+     * 저장된 배열을 순회하면서 숫자 문자가 아닌경우와 연산인 경우를 분리하여 생각한다.
+     * 숫자 문자가 아닌 경우(연산문자)인 경우 따로 연산 변수에 저장
+     * 숫자 문자인 경우 숫자 변수(before) 에 저장
+     * 숫자 문자 이면서 연산 변수에 저장된 값이 있는 경우 이전 숫자(before) 해당 되는 연산을 현재 숫자에 적용함.
+     */
+
     @Test
     void splitStringBySpace() {
         int before = 0;
-        String value = "2 + 3 + 5 + 5 * 2";
+        String value = "2 + 3 * 4 / 2";
         String operation = "";
         String[] values = value.split(" ");
         for (String v: values) {
@@ -20,6 +29,12 @@ class StringCalculatorTest {
                 case "*":
                     operation = "*";
                     break;
+                case "/":
+                    operation = "/";
+                    break;
+                case "-":
+                    operation = "-";
+                    break;
                 default:
                     int current = Integer.parseInt(v);
                     if (operation == "*") {
@@ -28,6 +43,16 @@ class StringCalculatorTest {
                     }
                     if (operation == "+") {
                         before += current;
+                        break;
+                    }
+
+                    if (operation == "/") {
+                        before /= current;
+                        break;
+                    }
+
+                    if(operation == "-"){
+                        before -= current;
                         break;
                     }
                     before = current;
